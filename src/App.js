7a542f1c9b602e2add2f0051932c435933afff65
Scorpion2on <skrypnychenko.s@gmail.com>
2020-08-19 21:19:15 +0200

    hide routes

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { ThemeProvider } from "@material-ui/styles";
 import theme from "./theme";
-import { BrowserRouter } from "react-router-dom";
-import Routes from "./Routes";
+// import { BrowserRouter } from "react-router-dom";
+// import Routes from "./Routes";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
+      {/* <BrowserRouter>
         <Routes />
-      </BrowserRouter>
+      </BrowserRouter> */}
     </ThemeProvider>
   );
 };
--- src/Routes.js
+++ src/Routes.js
@@ -1,20 +1,20 @@
-import React from "react";
-import { Switch } from "react-router-dom";
-import { Home, LoginPage } from "pages";
-import { GuestRoute } from "components/routes";
+// import React from "react";
+// import { Switch } from "react-router-dom";
+// import { Home, LoginPage } from "pages";
+// import { GuestRoute } from "components/routes";
 
-const Routes = () => {
-  return (
-    <Switch>
-      <GuestRoute exact 
-      component={Home} 
-      path="/" />
-      <GuestRoute
-        component={LoginPage}
-        path="/login"
-      />
-    </Switch>
-  );
-};
+// const Routes = () => {
+//   return (
+//     <Switch>
+//       <GuestRoute exact 
+//       component={Home} 
+//       path="/" />
+//       <GuestRoute
+//         component={LoginPage}
+//         path="/login"
+//       />
+//     </Switch>
+//   );
+// };
 
-export default Routes;
+// export default Routes;
